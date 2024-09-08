f1a66bf08976cc94a058b00e9f687c08202cbe6c
YashRawat0947 <147089324+YashRawat0947@users.noreply.github.com>
2024-09-09 00:08:13 +0530

    asv

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
@@ -20,6 +22,14 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   connectDB();
