cbd4e595201c5fb93eda8651464cc0215a55b8dd
YashRawat0947 <147089324+YashRawat0947@users.noreply.github.com>
2024-09-09 10:09:06 +0530

    wevwds

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import path from "path";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve()
